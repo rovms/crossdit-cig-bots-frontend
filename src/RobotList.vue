acac64edec24cd706e44cf1b367740970ff5b3bd
rovms <romabucher.afc@gmail.com>
2020-10-31 10:37:07 +0100

    fetch robots

---
+++ src/RobotList.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-container>
+    <v-card v-for="robot in robots" :key="robot"></v-card>
+  </v-container>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    robots: [];
+  },
+  created() {
+    console.log("created");
+    axios
+      .get("http://localhost:5000/api/robot")
+      .then((response) => {
+        this.robots = response.data;
+      })
+      .catch((error) => {
+        alert(error);
+      });
+  },
+};
+</script>
+()
+
+<style></style>
