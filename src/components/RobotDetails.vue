b7c9e28242c5649d543ec1dd0c26c5f877dd92c4
rovms <romabucher.afc@gmail.com>
2020-11-04 19:07:04 +0100

    marker stuff

--- src/components/RobotDetails.vue
+++ src/components/RobotDetails.vue
@@ -46,21 +46,30 @@
           <v-list-item-title>Trash level</v-list-item-title>
           <v-list-item-subtitle class="text-right"> {{ robot.trashLevel || 0 }} % </v-list-item-subtitle>
         </v-list-item>
-
-        <v-list-group>
-          <template v-slot:activator>
-            <v-list-item-content>
-              <v-list-item-title class="font-italic">Show on map</v-list-item-title>
-            </v-list-item-content>
-          </template>
-          <v-list-item>
-            <GmapMap :center="robot.position" :zoom="14" map-type-id="terrain" style="width: 400px; height: 500px">
-              <GmapMarker :position="robot.position" />
-              <GmapMarker v-for="(cc, i) in robot.cigsCollected" :key="i" :position="cc.position" />
-            </GmapMap>
-          </v-list-item>
-        </v-list-group>
       </v-list>
+      <div style="height: 350px;">
+        <LMap
+          style="height: 100%; width: 100%"
+          :zoom="zoom"
+          :center="robotPosition"
+          @update:zoom="zoomUpdated"
+          @update:center="centerUpdated"
+          @update:bounds="boundsUpdated"
+        >
+          <LTileLayer :url="url"></LTileLayer>
+          <LMarker :lat-lng="robotPosition"></LMarker>
+          <LCircleMarker
+            v-for="cigPos in cigarettes"
+            :key="cigPos.id"
+            :lat-lng="cigPos"
+            :radius="6"
+            color="red"
+            @click="move(cigPos)"
+          ></LCircleMarker>
+        </LMap>
+      </div>
+      {{ robotPosition }}
+
       <v-divider></v-divider>
       <v-card-actions>
         <v-btn small color="primary">Send to</v-btn>
@@ -72,11 +81,80 @@
 
 <script>
 import axios from "axios";
+import { LMap, LTileLayer, LMarker, LCircleMarker } from "vue2-leaflet";
+
 export default {
+  components: {
+    LMap,
+    LTileLayer,
+    LMarker,
+    LCircleMarker,
+  },
+
   data() {
     return {
       robot: {},
+      targetPosition: [],
+      cigarettes: [
+        [55.6603, 12.6013],
+        [55.6604, 12.6014],
+        [55.6603, 12.6016],
+        [55.6603, 12.6003],
+        [55.6606, 12.6011],
+      ],
+
+      robotPosition: [55.6607, 12.6015],
+
+      speed: 0.0001,
+
+      url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+      zoom: 14,
+      center: [],
+      bounds: null,
     };
+  },
+
+  methods: {
+    zoomUpdated(zoom) {
+      this.zoom = zoom;
+    },
+    centerUpdated(center) {
+      this.center = center;
+    },
+    boundsUpdated(bounds) {
+      this.bounds = bounds;
+    },
+
+    move(cigarettePosition) {
+      const diff0 = this.robotPosition[0] - cigarettePosition[0];
+      console.log("cigarettePosition");
+      console.log(cigarettePosition);
+      console.log(this.robotPosition);
+      let moved = false;
+      if (Math.abs(diff0) > 0.00015) {
+        moved = true;
+        if (diff0 > 0) {
+          this.robotPosition[0] -= this.speed;
+        } else if (diff0 < 0) {
+          this.robotPosition[0] += this.speed;
+        }
+      }
+
+      const diff1 = this.robotPosition[1] - cigarettePosition[1];
+      if (Math.abs(diff1) > 0.00015) {
+        moved = true;
+        if (diff1 > 0) {
+          this.robotPosition[1] -= this.speed;
+        } else if (diff1 < 0) {
+          this.robotPosition[1] += this.speed;
+        }
+      }
+
+      console.log(moved);
+      console.log(this.robotPosition);
+
+      if (moved) setTimeout(this.move(cigarettePosition), 5000);
+    },
   },
 
   filters: {
@@ -90,6 +168,7 @@ export default {
       .get("http://localhost:5000/api/robot/" + this.$route.params.robotId)
       .then((response) => {
         this.robot = response.data;
+        this.center = this.robot.position;
       })
       .catch((error) => {
         alert(error);
--- src/main.js
+++ src/main.js
@@ -5,6 +5,16 @@ import VueAxios from "vue-axios";
 import vuetify from "./plugins/vuetify";
 import router from "./router";
 import * as VueGoogleMaps from "vue2-google-maps";
+import "leaflet/dist/leaflet.css";
+
+import { Icon } from "leaflet";
+
+delete Icon.Default.prototype._getIconUrl;
+Icon.Default.mergeOptions({
+  iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+  iconUrl: require("leaflet/dist/images/marker-icon.png"),
+  shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
+});
 
 Vue.use(VueGoogleMaps, {
   load: {
