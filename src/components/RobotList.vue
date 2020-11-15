1cdbc2f45d792f3356615176099e15bbdfa5b660
Roman Bucher <romanbucher.afc@gmail.com>
2020-11-15 10:09:07 +0100

    Revert Dev
    This reverts commit d296e1a4467a626bc6b3e2e05c1fe36b4de7377c.

---
+++ src/components/RobotList.vue
@@ -0,0 +1,58 @@
+<template>
+  <v-container>
+    <v-card v-for="robot in robots" :key="robot.id" class="ma-1">
+      <v-card-title class="pb-0">
+        {{ robot.name }}
+      </v-card-title>
+      <v-list class="transparent">
+        <v-list-item>
+          <v-list-item-title>Energy used</v-list-item-title>
+          <v-list-item-icon>
+            <v-icon>mdi-chart-donut</v-icon>
+          </v-list-item-icon>
+          <v-list-item-subtitle class="text-right">
+            {{ robot.energyUsed || 0 }}
+          </v-list-item-subtitle>
+        </v-list-item>
+        <v-list-item>
+          <v-list-item-title>Battery level</v-list-item-title>
+          <v-list-item-icon>
+            <v-icon color="green">mdi-battery</v-icon>
+          </v-list-item-icon>
+          <v-list-item-subtitle class="text-right">
+            {{ robot.batteryLevel || 0 }}
+          </v-list-item-subtitle>
+        </v-list-item>
+      </v-list>
+      <v-divider></v-divider>
+      <v-card-actions>
+        <v-btn color="primary" :to="{ name: 'RobotDetails', params: { robotId: robot.id } }">Details</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      robots: [],
+    };
+  },
+  created() {
+    console.log("created");
+    axios
+      .get("http://localhost:5000/api/robot")
+      .then((response) => {
+        this.robots = response.data;
+      })
+      .catch((error) => {
+        alert(error);
+      });
+  },
+};
+</script>
+()
+
+<style></style>
--- src/main.js
+++ src/main.js
@@ -4,15 +4,13 @@ import axios from "axios";
 import VueAxios from "vue-axios";
 import vuetify from "./plugins/vuetify";
 import router from "./router";
-import "leaflet/dist/leaflet.css";
+import * as VueGoogleMaps from "vue2-google-maps";
 
-import { Icon } from "leaflet";
-
-delete Icon.Default.prototype._getIconUrl;
-Icon.Default.mergeOptions({
-  iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
-  iconUrl: require("leaflet/dist/images/marker-icon.png"),
-  shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: "AIzaSyD0hqCUKkcBOyypNAqiOvFqaQShO0FWhys",
+    libraries: "places", // This is required if you use the Autocomplete plugin
+  },
 });
 
 Vue.config.productionTip = false;
@@ -20,8 +18,6 @@ Vue.config.productionTip = false;
 Vue.use(VueAxios, axios);
 
 Vue.use(require("vue-moment"));
-
-// Vue.prototype.$http = VueAxios;
 
 new Vue({
   vuetify,
