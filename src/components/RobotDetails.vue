79058744b06b4f9cbbff65bc247f1fabecc28a55
Roman Bucher <romanbucher.afc@gmail.com>
2020-11-15 10:11:27 +0100

    Dev
    * init
    
    * works
    
    * details
    
    * stuff
    
    * scheduled move
    
    * change time
    
    * added env file
    
    * remove unused
    
    * added static json
    
    Co-authored-by: rovms <romabucher.afc@gmail.com>

--- src/components/RobotDetails.vue
+++ src/components/RobotDetails.vue
@@ -1,13 +1,44 @@
 <template>
-  <v-container>
+  <div>
+    <v-dialog v-model="assignEngineerDialog" max-width="600px">
+      <v-card>
+        <v-card-title>
+          <span class="headline">Notify company</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-select
+              v-model="selectedEngineer"
+              outlined
+              :items="engineerOptions"
+              label="Contact"
+              item-text="name"
+              return-object
+            ></v-select>
+            <v-textarea outlined label="Additional information" v-model="notifyReason"></v-textarea>
+          </v-container>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" text @click="assignEngineerDialog = false">
+            Close
+          </v-btn>
+          <v-btn color="blue darken-1" text @click="pickup">
+            Save & Send notification
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
     <v-card>
       <v-card-title class="pb-0"> ID: {{ robot.id }} </v-card-title>
       <v-list class="transparent">
         <v-list-item>
           <v-list-item-title>Status</v-list-item-title>
-          <v-list-item-subtitle class="text-right green--text"> {{ robot.status }} </v-list-item-subtitle>
+          <v-list-item-subtitle :class="robot.status !== 'Active' ? 'text-right red--text' : 'text-right green--text'">
+            {{ robot.status }}
+          </v-list-item-subtitle>
         </v-list-item>
-
         <v-list-item>
           <v-list-item-title>Energy used</v-list-item-title>
           <v-list-item-icon>
@@ -19,14 +50,12 @@
         <v-list-group>
           <template v-slot:activator>
             <v-list-item-content>
-              <v-list-item-title class="font-italic">Cigs collected</v-list-item-title>
+              <v-list-item-title class="font-italic">Last trash collected</v-list-item-title>
             </v-list-item-content>
           </template>
-          <v-list-item v-for="(cc, i) in robot.cigsCollected" :key="i">
-            <v-list-item-title>{{ cc.date | moment("DD.MM, h:mm") }}</v-list-item-title>
-            <v-list-item-subtitle class="text-right">
-              {{ cc.position.lat | toFixedNumber }} / {{ cc.position.lng | toFixedNumber }}
-            </v-list-item-subtitle>
+          <v-list-item v-for="(t, i) in trash" :key="i">
+            <v-list-item-title>{{ t.date | moment("DD.MM, h:mm") }}</v-list-item-title>
+            <v-list-item-subtitle class="text-right"> {{ t.weight }} g </v-list-item-subtitle>
           </v-list-item>
         </v-list-group>
 
@@ -34,6 +63,30 @@
           <v-list-item-title>Installation</v-list-item-title>
           <v-list-item-subtitle class="text-right">
             {{ robot.installationAt | moment("DD.MM.YYYY") }}
+          </v-list-item-subtitle>
+        </v-list-item>
+        <v-list-item>
+          <v-list-item-title>Oil</v-list-item-title>
+          <v-list-item-subtitle class="text-right">
+            {{ robot.oil }}
+          </v-list-item-subtitle>
+        </v-list-item>
+        <v-list-item>
+          <v-list-item-title>Wheels</v-list-item-title>
+          <v-list-item-subtitle :class="robot.wheels !== 'Ok' ? 'text-right red--text' : 'text-right'">
+            {{ robot.wheels }}
+          </v-list-item-subtitle>
+        </v-list-item>
+        <v-list-item>
+          <v-list-item-title>Camera</v-list-item-title>
+          <v-list-item-subtitle class="text-right">
+            {{ robot.camera }}
+          </v-list-item-subtitle>
+        </v-list-item>
+        <v-list-item>
+          <v-list-item-title>Company</v-list-item-title>
+          <v-list-item-subtitle class="text-right">
+            {{ selectedEngineer ? selectedEngineer.name : "" }}
           </v-list-item-subtitle>
         </v-list-item>
 
@@ -47,52 +100,43 @@
           <v-list-item-subtitle class="text-right"> {{ robot.trashLevel || 0 }} % </v-list-item-subtitle>
         </v-list-item>
       </v-list>
-      <div style="height: 350px;">
-        <LMap
-          style="height: 100%; width: 100%"
-          :zoom="zoom"
-          :center="robotPosition"
-          @update:zoom="zoomUpdated"
-          @update:center="centerUpdated"
-          @update:bounds="boundsUpdated"
-        >
-          <LTileLayer :url="url"></LTileLayer>
-          <LMarker :lat-lng="robotPosition"></LMarker>
-          <LCircleMarker
-            v-for="cigPos in cigarettes"
-            :key="cigPos.id"
-            :lat-lng="cigPos"
-            :radius="6"
-            color="red"
-            @click="move(cigPos)"
-          ></LCircleMarker>
-        </LMap>
-      </div>
-      {{ robotPosition }}
 
-      <v-divider></v-divider>
       <v-card-actions>
-        <v-btn @click="pickup" small color="warning">Pick up</v-btn>
+        <v-spacer></v-spacer>
+        <v-btn text @click="assignEngineerDialog = true" small color="primary">Notify engineer</v-btn>
       </v-card-actions>
     </v-card>
-  </v-container>
+  </div>
 </template>
 
 <script>
 import axios from "axios";
-import { LMap, LTileLayer, LMarker, LCircleMarker } from "vue2-leaflet";
-import authHeader from "@/auth/authHeader";
+
+const trash = [
+  {
+    weight: 27,
+    date: new Date(),
+  },
+  {
+    weight: 41,
+    date: new Date(),
+  },
+  {
+    weight: 57,
+    date: new Date(),
+  },
+];
 
 export default {
-  components: {
-    LMap,
-    LTileLayer,
-    LMarker,
-    LCircleMarker,
-  },
-
   data() {
     return {
+      assignEngineerDialog: false,
+      selectedEngineer: "",
+      engineerOptions: [],
+      notifyReasonDetail: "",
+
+      map: null,
+      trash,
       robot: {},
       targetPosition: [],
       cigarettes: [
@@ -109,71 +153,76 @@ export default {
 
       url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
       zoom: 14,
-      center: [],
+      center: [55.6603, 12.6013],
       bounds: null,
     };
   },
 
   methods: {
+    mapReady() {
+      console.log("hello");
+      this.map = this.$refs.robotmap.mapObject;
+    },
+
     pickup() {
-      console.log("pickup");
-      axios.post(
-        "http://localhost:5000/api/robot/pickup",
-        {
+      axios
+        .post("http://localhost:5000/api/robot/pickup", {
           robotId: this.$route.params.robotId,
-        },
-        { headers: authHeader() }
-      );
+          engineerId: this.selectedEngineer._id,
+        })
+        .then((response) => {
+          console.log(response);
+          if (response.status === 200) console.log("ok");
+        })
+        .catch((error) => {
+          console.log(error);
+          alert(error);
+        });
+
+      this.assignEngineerDialog = false;
     },
 
-    move(cigarettePosition) {
-      const diff0 = this.robotPosition[0] - cigarettePosition[0];
-      console.log("cigarettePosition");
-      console.log(cigarettePosition);
-      console.log(this.robotPosition);
-      let moved = false;
-      if (Math.abs(diff0) > 0.00015) {
-        moved = true;
-        if (diff0 > 0) {
-          this.robotPosition[0] -= this.speed;
-        } else if (diff0 < 0) {
-          this.robotPosition[0] += this.speed;
-        }
-      }
-
-      const diff1 = this.robotPosition[1] - cigarettePosition[1];
-      if (Math.abs(diff1) > 0.00015) {
-        moved = true;
-        if (diff1 > 0) {
-          this.robotPosition[1] -= this.speed;
-        } else if (diff1 < 0) {
-          this.robotPosition[1] += this.speed;
-        }
-      }
-
-      console.log(moved);
-      console.log(this.robotPosition);
-
-      if (moved) setTimeout(this.move(cigarettePosition), 5000);
+    fetchEngineers() {
+      axios
+        .get("http://localhost:5000/api/engineer")
+        .then((response) => {
+          this.engineerOptions = response.data;
+        })
+        .catch((error) => alert(error));
     },
-  },
 
-  filters: {
-    toFixedNumber(number) {
-      return Math.round(number * 1e5) / 1e5;
+    fetchRobot() {
+      axios
+        .get("http://localhost:5000/api/robot/" + this.$route.params.robotId)
+        .then((response) => {
+          this.robot = response.data;
+          console.log(response.data);
+          this.selectedEngineer = response.data.engineer;
+        })
+        .catch((error) => {
+          alert(error);
+        });
     },
   },
 
   created() {
-    axios
-      .get("http://localhost:5000/api/robot/" + this.$route.params.robotId, { headers: authHeader() })
-      .then((response) => {
-        this.robot = response.data;
-        this.center = this.robot.position;
-      })
-      .catch((error) => {
-        alert(error);
-      });
+    this.fetchEngineers();
+    this.fetchRobot();
+  },
+
+  computed: {
+    notifyReason() {
+      if (this.selected && this.selected[0]) {
+        const robot = this.selected[0];
+        console.log(robot);
+        let errorMessage = "";
+        if (robot.wheels !== "Ok") errorMessage += robot.wheels + "\n";
+        if (robot.oil !== "Ok") errorMessage += robot.oil + "\n";
+        if (robot.camera !== "Ok") errorMessage += robot.camera + "\n";
+        return errorMessage + "\n\n" + this.notifyReasonDetail;
+      }
+      return "";
+    },
   },
 };
 </script>
